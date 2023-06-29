a89b21dd0034d5f9246f1d7e720d3b4b6820006e
Shimabukuro Sora <s23012@std.it-college.ac.jp>
2023-06-29 14:29:25 +0000

    images/

---
+++ images/glasses.py
@@ -0,0 +1,11 @@
+def find_middle_number(numbers):
+    sorted_numbers = sorted(numbers, reverse=True)
+    middle_index = (len(sorted_numbers) + 1) // 2
+    return str(sorted_numbers[middle_index - 1])
+
+
+n = int(input())
+numbers = list(map(int, input().split()))
+
+result = find_middle_number(numbers)
+print(result)
