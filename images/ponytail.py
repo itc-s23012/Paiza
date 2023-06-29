f46f224d04ddf4bf0ba70e95c311eb95f9ad590f
Shimabukuro Sora <s23012@std.it-college.ac.jp>
2023-06-29 14:09:32 +0000

    images/

---
+++ images/ponytail.py
@@ -0,0 +1,13 @@
+def check_vision_test_results(results):
+    correct_count = sum(
+        [1 for result in results if result.split()[0] == result.split()[1]]
+    )
+    return "OK" if correct_count >= 3 else "NG"
+
+
+# 入力を受け取る
+results = [input() for _ in range(5)]
+
+# 結果を出力
+result = check_vision_test_results(results)
+print(result)
